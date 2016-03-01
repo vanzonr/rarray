9faac90407a8ecc324790d8309138fb34bac9f3e
Ramses van Zon <vanzonr@gmail.com>
2016-02-29 22:26:38 -0500

    Added newlines to rarrayio streaming operators

--- rarraytests.cc
+++ rarraytests.cc
@@ -23,6 +23,8 @@
 //
 #define BOOST_TEST_MODULE RarrayTests
 #include "boost/test/included/unit_test.hpp"
+//#define BOOST_TEST_DYN_LINK
+//#include "boost/test/unit_test.hpp"
 #include <boost/test/test_case_template.hpp>
 #include <boost/mpl/list.hpp>
 
