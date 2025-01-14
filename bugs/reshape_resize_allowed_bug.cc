44b9269a42908b424136fb75422b9d06cd6ec55a
Ramses van Zon <vanzonr@gmail.com>
2025-01-13 21:18:24 -0500

    made reshape inconsistent size bug test code more verbose

--- bugs/reshape_resize_allowed_bug.cc
+++ bugs/reshape_resize_allowed_bug.cc
@@ -4,8 +4,37 @@ int main()
 {
     const int oldsize = 2, newsize = 1;
     rvector<double> a(oldsize);
+    auto oldbegin = a.begin();
+    auto oldend = a.end();
     a.reshape(newsize, ra::RESIZE::ALLOWED);
-    return (a.end() - a.begin() != a.size())
-        || (a.end() - a.begin() != newsize)
-        || (a.size() != newsize);
+    auto newbegin = a.begin();
+    auto newend = a.end();
+    auto reported_size = a.size();
+    auto measured_extent = a.end() - a.begin();
+    auto test1 = measured_extent == reported_size;
+    auto test2 = measured_extent == newsize;
+    auto test3 = reported_size == newsize;
+    auto test4 = newbegin == oldbegin;
+    auto test5 = newend != oldend;
+    if (test1)
+        std::cerr << "RIGHT: measured_extent == reported_size\n";
+    else
+        std::cerr << "WRONG: measured_extent != reported_size\n";
+    if (test2)
+        std::cerr << "RIGHT: measured_extent == newsize\n";
+    else
+        std::cerr << "WRONG: measured_extent != newsize\n";
+    if (test3) 
+        std::cerr << "RIGHT: reported_size == newsize\n";
+    else
+        std::cerr << "WRONG: reported_size != newsize\n";
+    if (test4) 
+        std::cerr << "RIGHT: newbegin == oldbegin\n";
+    else
+        std::cerr << "WRONG: newbegin != oldbegin\n";
+    if (test5) 
+        std::cerr << "RIGHT: newend != oldend\n";
+    else
+        std::cerr << "WRONG: newend == oldend\n";
+    return not (test1 and test2 and test3 and test4 and test5);
 }
